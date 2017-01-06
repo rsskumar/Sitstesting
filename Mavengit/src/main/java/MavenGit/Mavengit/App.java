2689be37bd1ae37878fc933f5b0506992cc0ec10
sreddi <sreddi@MDC-LT13648.loe.corp>
2017-01-06 14:08:35 -0500

    commit changes

---
+++ Mavengit/src/main/java/MavenGit/Mavengit/App.java
@@ -0,0 +1,13 @@
+package MavenGit.Mavengit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Mavengit/src/test/java/MavenGit/Mavengit/TestMercuryPage.java
@@ -0,0 +1,17 @@
+package MavenGit.Mavengit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestMercuryPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	WebDriver driver = new FirefoxDriver();
+	driver.get("http://www.fda.gov/");
+	System.out.println("FDA PAGE IS DISPLAYED");
+	System.out.println("FDA PAGE IS DISPLAYED");
+		}
+
+}
